9d7fcab3aca805b14b84193bfb4ee9771800d393
Rajeev Roushan <rajeevr@vervelogic.com>
2017-09-08 16:24:36 +0530

    first commit

---
+++ ApiClient.cs
@@ -0,0 +1,121 @@
+﻿using HttpHandler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IgdbAPI
+{
+    public class ApiClient
+    {
+        Client client = null;
+
+        /// <summary>
+        /// Initialize Client with Base Url of API and user-key
+        /// </summary>
+        /// <param name="api_base_url">API base Url</param>
+        public ApiClient(string api_base_url)
+        {
+            client = new Client(api_base_url);
+            
+        }
+
+        /// <summary>
+        /// Set API Auth Header if required
+        /// </summary>
+        /// <param name="key">Header Key</param>
+        /// <param name="value">Header Value</param>
+        public void SetAuthHeader(string key, string value)
+        {
+            client.AddHeader(key, value);
+        }
+
+        public async Task<List<Game>> GetGamesAsync()
+        {
+            return await client.GetDataAsync<List<Game>>("games/?fields=*&limit=50");
+        }
+
+        public async Task<Game> GetGameAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<Game>>(string.Format("games/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Platform>> GetPlatformsAsync()
+        {
+            return await client.GetDataAsync<List<Platform>>("platforms/?fields=*&limit=50");
+        }
+
+        public async Task<Platform> GetPlatformAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<Platform>>(string.Format("platforms/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Game>> GetPlatformGamesAsync(long id)
+        {
+            return await client.GetDataAsync<List<Game>>(string.Format("platforms/{0}/games/?fields=*", id));
+        }
+
+        public async Task<List<Collection>> GetCollectionsAsync()
+        {
+            return await client.GetDataAsync<List<Collection>>("collections/?fields=*&limit=50");
+        }
+
+        public async Task<Collection> GetCollectionAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<Collection>>(string.Format("collections/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Company>> GetCompaniesAsync()
+        {
+            return await client.GetDataAsync<List<Company>>("companies/?fields=*&limit=50");
+        }
+
+        public async Task<Company> GetCompanyAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<Company>>(string.Format("companies/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Game>> GetCompanyGamesAsync(long id)
+        {
+            return await client.GetDataAsync<List<Game>>(string.Format("companies/{0}/games/?fields=*", id));
+        }
+
+
+        public async Task<List<People>> GetPeoplesAsync()
+        {
+            return await client.GetDataAsync<List<People>>("people/?fields=*&limit=50");
+        }
+
+        public async Task<People> GetPeopleAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<People>>(string.Format("people/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Game>> GetPeopleGamesAsync(long id)
+        {
+            return await client.GetDataAsync<List<Game>>(string.Format("people/{0}/games/?fields=*", id));
+        }
+
+
+        public async Task<List<Franchise>> GetFranchisesAsync()
+        {
+            return await client.GetDataAsync<List<Franchise>>("franchises/?fields=*&limit=50");
+        }
+
+        public async Task<Franchise> GetFranchiseAsync(long id)
+        {
+            var result = await client.GetDataAsync<List<Franchise>>(string.Format("franchises/{0}/?fields=*", id));
+            return result == null ? null : result.FirstOrDefault();
+        }
+
+        public async Task<List<Game>> GetFranchiseGamesAsync(long id)
+        {
+            return await client.GetDataAsync<List<Game>>(string.Format("franchises/{0}/games/?fields=*", id));
+        }
+
+    }
+}
---
+++ Modals.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.Generic;
+
+namespace IgdbAPI
+{
+    public class BasicEntity
+    {
+        public long id { get; set; }
+
+        public string name { get; set; }
+        public string slug { get; set; }
+        public string url { get; set; }
+
+        public long created_at { get; set; }
+        public long updated_at { get; set; }
+
+        public List<long> games { get; set; }
+
+        public BasicEntity()
+        {
+            games = new List<long>();
+        }
+    }
+
+    public class Rating
+    {
+        public int rating { get; set; }
+        public string synopsis { get; set; }
+    }
+
+    public class Image_Base
+    {
+        public string url { get; set; }
+        public string cloudinary_id { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Game : BasicEntity
+    {
+        public string summary { get; set; }
+        public string storyline { get; set; }
+
+        public int hypes { get; set; }
+        public double rating { get; set; }
+        public double popularity { get; set; }
+        public double aggregated_rating { get; set; }
+        public int aggregated_rating_count { get; set; }
+        public double total_rating { get; set; }
+        public int total_rating_count { get; set; }
+        public int rating_count { get; set; }
+        public int category { get; set; }
+        public long collection { get; set; }
+        public long first_release_date { get; set; }
+        public int status { get; set; }
+
+
+
+        public List<long> tags { get; set; }
+        public List<long> themes { get; set; }
+        public List<long> genres { get; set; }
+        public List<long> keywords { get; set; }
+        public List<long> developers { get; set; }
+        public List<long> publishers { get; set; }
+        public List<long> game_modes { get; set; }
+        public List<long> game_engines { get; set; }
+
+        public PEGI pegi { get; set; }
+        public ESRB esrb { get; set; }
+        public Cover cover { get; set; }
+
+        public List<Website> websites { get; set; }
+        public List<Image> screenshots { get; set; }
+        public List<ReleaseDate> release_dates { get; set; }
+        public List<Alternative_name> alternative_names { get; set; }
+
+        public Game()
+        {
+            tags = new List<long>();
+            themes = new List<long>();
+            genres = new List<long>();
+            keywords = new List<long>();
+            developers = new List<long>();
+            publishers = new List<long>();
+            game_modes = new List<long>();
+            game_engines = new List<long>();
+
+            pegi = new PEGI();
+            esrb = new ESRB();
+            cover = new Cover();
+            websites = new List<Website>();
+            screenshots = new List<Image>();
+            release_dates = new List<ReleaseDate>();
+            alternative_names = new List<Alternative_name>();
+        }
+    }
+
+
+    public class People : BasicEntity
+    {
+
+    }
+
+    public class Franchise : BasicEntity
+    {
+
+    }
+
+    public class Company : BasicEntity
+    {
+        public Image logo { get; set; }
+        public string description { get; set; }
+        public int country { get; set; }
+        public string website { get; set; }
+
+        public long start_date { get; set; }
+        public int start_date_category { get; set; }
+        public long changed_company_id { get; set; }
+        public long change_date { get; set; }
+        public int change_date_category { get; set; }
+
+        public string twitter { get; set; }
+        public string facebook { get; set; }
+
+        public List<long> published { get; set; }
+        public List<long> developed { get; set; }
+
+        public Company()
+        {
+            logo = new Image();
+            published = new List<long>();
+            developed = new List<long>();
+        }
+
+    }
+
+    public class Game_Engine : BasicEntity
+    {
+        public Image logo { get; set; }
+        public List<long> platforms { get; set; }
+        public List<long> companies { get; set; }
+
+        public Game_Engine()
+        {
+            logo = new Image();
+            platforms = new List<long>();
+            companies = new List<long>();
+        }
+    }
+
+    public class Website
+    {
+        public string url { get; set; }
+        public int category { get; set; }
+    }
+
+    public class Cover : Image_Base
+    {
+
+    }
+
+    public class ESRB : Rating
+    {
+
+    }
+
+    public class PEGI : Rating
+    {
+
+    }
+
+    public class Alternative_name
+    {
+        public string name { get; set; }
+        public string comment { get; set; }
+    }
+
+    public class Image : Image_Base
+    {
+
+    }
+
+    public class Collection : BasicEntity
+    {
+
+    }
+
+    public class ReleaseDate
+    {
+        public long id { get; set; }
+        public long game { get; set; }
+        public int category { get; set; }
+        public long platform { get; set; }
+        public string human { get; set; }
+        public long created_at { get; set; }
+        public long updated_at { get; set; }
+        public long date { get; set; }
+        public int region { get; set; }
+        public int y { get; set; }
+        public int m { get; set; }
+    }
+
+    public class Genre : BasicEntity
+    {
+
+    }
+
+    public class Keyword : BasicEntity
+    {
+
+    }
+
+    public class Platform : BasicEntity
+    {
+        public string shortcut { get; set; }
+        public string website { get; set; }
+        public string summary { get; set; }
+        public string alternative_name { get; set; }
+        public Image logo { get; set; }
+
+        public Platform()
+        {
+            logo = new Image();
+        }
+    }
+
+    public class Theme : BasicEntity
+    {
+
+    }
+}
